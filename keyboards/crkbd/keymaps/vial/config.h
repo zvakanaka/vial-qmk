de907f2e87d814c3ba0df4091b242d3ceabef5c2
zvakanaka <8365885+zvakanaka@users.noreply.github.com>
2024-09-26 23:30:39 -0400

    support Cygnus 1.0 3x5 (no RGBLIGHT)

--- keyboards/crkbd/keymaps/vial/config.h
+++ keyboards/crkbd/keymaps/vial/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define VIAL_KEYBOARD_UID {0x3B, 0x6B, 0xA0, 0x29, 0x80, 0x56, 0xED, 0xD1}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
-#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {1, 2}
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define TAPPING_TERM 180
--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -2,7 +2,7 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
-RGBLIGHT_ENABLE     = yes
+RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
