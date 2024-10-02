585a25e946bb6da3e637fd8a59e13e31a78fb23f
zvakanaka <8365885+zvakanaka@users.noreply.github.com>
2024-10-02 00:10:19 -0400

    mouse, media, and macro keys

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -4,10 +4,12 @@ LTO_ENABLE          = yes
 
 RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
-MOUSEKEY_ENABLE     = no
+MOUSEKEY_ENABLE     = yes
 OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = no
+EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
+
+DYNAMIC_MACRO_ENABLE = yes
